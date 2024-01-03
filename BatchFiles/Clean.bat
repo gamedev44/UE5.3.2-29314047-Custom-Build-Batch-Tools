afa9e21f8ada21dd9cf22a04665f70f43dafc89d
gamedev44 <108845184+gamedev44@users.noreply.github.com>
2024-01-03 01:01:31 -0600

    Updated  Clean.bat
    Modifications and Features:
    
    Error Logging: The script now logs errors to a "Logfile.txt" file located in an "Error Logs" folder in the same directory as the script.
    Error Checking: After calling Build.bat with the -Clean parameter, the script checks if the operation was successful. If there's an error, it logs this to the logfile.
    Folder Creation: The script creates the "Error Logs" folder if it doesn't already exist.

--- BatchFiles/Clean.bat
+++ BatchFiles/Clean.bat
@@ -1,23 +1,22 @@
 @echo off
-
-rem ## Unreal Engine code cleanup script
-rem ## Copyright Epic Games, Inc. All Rights Reserved.
-rem ##
-rem ## This script is expecting to exist in the Engine/Build/BatchFiles directory.  It will not work correctly
-rem ## if you copy it to a different location and run it.
-rem ##
-rem ##     %1 is the game name
-rem ##     %2 is the platform name
-rem ##     %3 is the configuration name
-rem ##     additional args are passed directly to UnrealBuildTool
+setlocal
 
 IF NOT EXIST "%~dp0\Build.bat" GOTO Error_MissingBuildBatchFile
 
 call "%~dp0\Build.bat" %* -Clean
+IF %ERRORLEVEL% NEQ 0 CALL :LogError "Error during build clean operation."
 goto Exit
 
 :Error_MissingBuildBatchFile
-ECHO Build.bat not found in "%~dp0"
+CALL :LogError "Build.bat not found in '%~dp0'."
 EXIT /B 999
 
 :Exit
+exit /b
+
+:LogError
+set "ErrorMsg=%~1"
+if not exist "%~dp0Error Logs" mkdir "%~dp0Error Logs"
+echo %ErrorMsg% >> "%~dp0Error Logs\Logfile.txt"
+echo %ErrorMsg%
+goto :eof
