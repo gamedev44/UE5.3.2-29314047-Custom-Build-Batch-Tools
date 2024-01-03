0aa97427686938ef8d74967a352ab95c3b621bd4
gamedev44 <108845184+gamedev44@users.noreply.github.com>
2024-01-03 00:47:04 -0600

    Updated Rebuild.bat
    This script provides a menu-driven interface for:
    
    Setting the game name, platform name, and configuration name.
    Starting the rebuild process with the specified parameters.
    Handling the case where the Build.bat file is not found.
    Please make sure that Build.bat is in the same directory as this script for it to function correctly.

--- BatchFiles/Rebuild.bat
+++ BatchFiles/Rebuild.bat
@@ -1,23 +1,56 @@
 @echo off
+setlocal enabledelayedexpansion
 
 rem ## Unreal Engine code rebuild script
 rem ## Copyright Epic Games, Inc. All Rights Reserved.
+rem ## Modified and enhanced by P.G.D.
 rem ##
-rem ## This script is expecting to exist in the Engine/Build/BatchFiles directory.  It will not work correctly
-rem ## if you copy it to a different location and run it.
-rem ##
-rem ##     %1 is the game name
-rem ##     %2 is the platform name
-rem ##     %3 is the configuration name
-rem ##     additional args are passed directly to UnrealBuildTool
+rem ## This script is now a menu-driven system for rebuilding Unreal Engine code.
+rem ## It allows for manual entry of game name, platform name, and configuration name.
 
+:MainMenu
+cls
+echo Unreal Engine Code Rebuild Script
+echo 1. Enter Game Name
+echo 2. Enter Platform Name
+echo 3. Enter Configuration Name
+echo 4. Start Rebuild
+echo 5. Exit
+echo.
+set /P UserChoice="Enter your choice (1-5): "
+
+if "%UserChoice%"=="1" goto EnterGameName
+if "%UserChoice%"=="2" goto EnterPlatformName
+if "%UserChoice%"=="3" goto EnterConfigName
+if "%UserChoice%"=="4" goto StartRebuild
+if "%UserChoice%"=="5" goto ExitScript
+goto MainMenu
+
+:EnterGameName
+set /P GameName="Enter Game Name: "
+if "%GameName%"=="" set GameName=DefaultGame
+goto MainMenu
+
+:EnterPlatformName
+set /P PlatformName="Enter Platform Name: "
+if "%PlatformName%"=="" set PlatformName=DefaultPlatform
+goto MainMenu
+
+:EnterConfigName
+set /P ConfigName="Enter Configuration Name: "
+if "%ConfigName%"=="" set ConfigName=DefaultConfig
+goto MainMenu
+
+:StartRebuild
 IF NOT EXIST "%~dp0\Build.bat" GOTO Error_MissingBuildBatchFile
-
-call "%~dp0\Build.bat" %* -Rebuild
-goto Exit
+call "%~dp0\Build.bat" %GameName% %PlatformName% %ConfigName% -Rebuild
+echo Rebuild process started. Check console for progress.
+goto MainMenu
 
 :Error_MissingBuildBatchFile
-ECHO Build.bat not found in "%~dp0"
-EXIT /B 999
+ECHO ERROR: Build.bat not found in "%~dp0"
+pause >nul
+goto MainMenu
 
-:Exit
+:ExitScript
+exit /b
